67ae716c8fc0597eac6679fc7cad3f9b6be37b55
InkretIvan <ivaninkret7@gmail.com>
2022-03-23 22:17:03 +0100

    react part 2

--- React/my-app/src/components/app.jsx
+++ React/my-app/src/components/app.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Footer from './footer';
 import Note from './note';
 import Header from './header';
+import notes from '../notes';
 
 function App(){
   return(
     <div>
     <Header />
-    <Note />
+    {console.log(notes)}
+    {notes.map(note => (<Note key={note.key} title={note.title} content={note.content} />))}
     <Footer />
     </div>
       );
